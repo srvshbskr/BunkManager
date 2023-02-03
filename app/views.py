31a1d6efd63582a5aed4a3f4d1899d78288d2e31
Srvsh <srvshbskr@gmail.com>
2023-02-03 14:29:37 +0530

    added templates system and basic views and urls

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def homepage(request):
+
+    context = {}
+    return render(request,'homepage.html',context)
--- main/settings.py
+++ main/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
