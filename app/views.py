089f59cc2c3a9237924486881aaa6a040bb278e1
Srvsh <srvshbskr@gmail.com>
2023-02-19 14:59:47 +0530

    added list page functions

--- app/views.py
+++ app/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render,redirect
-from .forms import CreateUserForm,createRecordForm
-from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, createRecordForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required,permission_required
+from django.contrib.auth.decorators import login_required, permission_required
 from .models import Record
+from django.contrib.auth import get_user_model
+
 
 def homepage(request):
 
     context = {}
-    return render(request,'homepage.html',context)
+    return render(request, 'homepage.html', context)
+
 
 def signuppage(request):
     form = CreateUserForm()
@@ -18,81 +21,84 @@ def signuppage(request):
             form.save()
             return redirect('home')
 
-    context={'form':form}
-    return render(request,'signuppage.html',context)
+    context = {'form': form}
+    return render(request, 'signuppage.html', context)
 
 
 def loginpage(request):
-    
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('Password')
-        user = authenticate(request,username = username,password = password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request,user) 
+            login(request, user)
             return redirect('report')
-        
+
         else:
-            messages.info(request,'invalid credentials')
-    context ={}
-    return render(request,'login.html',context)
+            messages.info(request, 'invalid credentials')
+    context = {}
+    return render(request, 'login.html', context)
 
 
 def logoutpage(request):
 
     logout(request)
-    return redirect('login')    
+    return redirect('login')
+
 
 @login_required(login_url='login')
 def reportpage(request):
-    r = Record.objects.filter(user =request.user)
-    p =0
+    r = Record.objects.filter(user=request.user)
+    p = 0
     a = 0
     t = 0
-    for i in r :
+    for i in r:
         t += 8
         if i.hour1:
-            p +=1
+            p += 1
         else:
-            a+=1
+            a += 1
         if i.hour2:
-            p +=1
+            p += 1
         else:
-            a+=1
+            a += 1
         if i.hour3:
-            p +=1
+            p += 1
         else:
-            a+=1
+            a += 1
         if i.hour4:
-            p +=1
+            p += 1
         else:
-            a+=1
+            a += 1
         if i.hour5:
-            p +=1
+            p += 1
         else:
-            a+=1
+            a += 1
         if i.hour6:
-            p +=1
+            p += 1
         else:
-            a+=1
+            a += 1
         if i.hour7:
-            p +=1
+            p += 1
         else:
-            a+=1
+            a += 1
         if i.hour8:
-            p +=1
+            p += 1
         else:
-            a+=1
+            a += 1
+    if t != 0:
+        per = p/t * 100
+    else:
+        per = 0
 
-        per = p/t *100
+    context = {'list': r, 't': t, 'a': a, 'p': p, 'pe': per}
+    return render(request, 'report.html', context)
 
-    context = {'list':r,'t':t,'a':a,'p':p}
-    return render(request,'report.html',context)
 
 def createrecordpage(request):
 
     form = createRecordForm()
-
 
     if request.method == 'POST':
         form = createRecordForm(request.POST)
@@ -100,8 +106,68 @@ def createrecordpage(request):
             form.save()
             return redirect('createrecord')
         else:
-            messages.info(request,'invalid Details')
-    context = {'form':form}
+            messages.info(request, 'invalid Details')
+    context = {'form': form}
 
-    return render(request,'createrecordpage.html',context)
+    return render(request, 'createrecordpage.html', context)
 
+
+def listpage(request):
+
+    user = get_user_model()
+    l = user.objects.all()
+
+    rl = {}
+    pl = {}
+    for i in l:
+        rl[i] = Record.objects.filter(user=i)
+    for x in rl:
+        print('X =', x)
+        p = 0
+        a = 0
+        t = 0
+        per = 0
+        temp = rl[x]
+        print(temp) 
+        for i in temp:
+            t += 8
+            if i.hour1:
+                p += 1
+            else:
+                a += 1
+            if i.hour2:
+                p += 1
+            else:
+                a += 1
+            if i.hour3:
+                p += 1
+            else:
+                a += 1
+            if i.hour4:
+                p += 1
+            else:
+                a += 1
+            if i.hour5:
+                p += 1
+            else:
+                a += 1
+            if i.hour6:
+                p += 1
+            else:
+                a += 1
+            if i.hour7:
+                p += 1
+            else:
+                a += 1
+            if i.hour8:
+                p += 1
+            else:
+                a += 1
+        if t != 0:
+            per = p/t * 100
+        else:
+            per = 0
+        pl[x] = per
+    print(pl)
+    context = {'pt':pl}
+    return render(request, 'list.html', context)
